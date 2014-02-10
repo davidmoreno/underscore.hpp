5d86b0e5d3280a06580a690eee7bd9d24bc145bb
David Moreno <dmoreno@coralbits.com>
2014-02-10 16:20:29 +0100

    Intiial version of streams api.

--- examples/services.cpp
+++ examples/services.cpp
@@ -1,14 +1,70 @@
 #include <fstream>
 #include <map>
 #include <iostream>
+#include <fstream>
 
 #include "../underscore.hpp"
+#include "../streams.hpp"
 
 using namespace underscore;
+
+void using_underscore();
+void using_streams();
+void manual();
 
 int main(int argc, char **argv){
 	// Convert it all to a map. If several ports per service, only one.
 	//std::map<std::string, int> 
+	//using_underscore();
+	using_streams();
+	manual();
+}
+
+void using_streams(){
+	stream<std::string> s;
+	
+	
+}
+
+
+void manual(){
+	std::ifstream input("/etc/services", std::ifstream::in);
+	std::map<std::string, int> map;
+	std::string str;
+	while (!input.eof()){
+		std::getline(input, str); // Get line
+		
+		str=str.substr(0, str.find_first_of('#')); // Remove comments, and trim
+		auto i=str.find_first_not_of(' '); auto end=str.find_last_not_of(' ');
+		if (i==end) 
+			str=std::string();
+		else
+			str=str.substr(i,end+1);
+		
+		if (str.empty()) // do not process empty lines
+			continue; 
+		
+		auto t=std::make_tuple<std::string,std::string>(str.substr(0,str.find_first_of(' ')), str.substr(str.find_last_of(' '))); // Create tuples <service, port/prot>
+		
+		if (std::get<0>(t).length()==0 || std::get<1>(t).length()==0) // Only tcp ports
+			continue; 
+		std::string port=std::get<1>(t);
+		if (port.size()<4 || port.substr(port.size()-4)!="/tcp") 
+			continue;
+		
+		auto p=std::make_tuple(std::get<0>(t), std::stoi( port.substr(0,port.size()-4) )); // Create <service, port> Pair
+		
+		map[std::get<0>(p)]=std::get<1>(p); // Create map.
+	}
+	
+	
+	for(auto &s: {"http","ssh","telnet"} )
+		std::cout<<s<<" at tcp port "<<map[s]<<std::endl;
+	;
+}
+
+
+void using_underscore(){
 	auto services= 
 		__(std::ifstream("/etc/services"))
 			.map<std::string>([](const std::string &s){ // Remove comments, and trim
